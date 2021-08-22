5e5b8f05c2f6b6f158436bad33495beea6408635
Michael Radwin <michael@radwin.org>
2021-08-22 13:34:08 -0700

    Use native Swift Date class instead of SimpleDate

--- Sources/Hebcal/greg.swift
+++ Sources/Hebcal/greg.swift
@@ -15,6 +15,8 @@ let monthLengths = [
     [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
 ]
 
+let gregCalendar = Calendar(identifier: .gregorian)
+
 public func isGregLeapYear(year: Int) -> Bool {
     return (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0)
 }
@@ -24,11 +26,12 @@ public func isGregLeapYear(year: Int) -> Bool {
  * For example, dayOfYear({1,1,1987}) returns the value 1
  * while dayOfYear({12,31,1980}) returns 366.
  */
-public func dayOfYear(date: SimpleDate) -> Int {
-    var days = date.dd + 31 * (date.mm - 1)
-    if date.mm > FEB {
-        days -= (4 * date.mm + 23) / 10
-        if isGregLeapYear(year: date.yy) {
+public func dayOfYear(dateComponents: DateComponents) -> Int {
+    let month = dateComponents.month!
+    var days = dateComponents.day! + 31 * (month - 1)
+    if month > FEB {
+        days -= (4 * month + 23) / 10
+        if isGregLeapYear(year: dateComponents.year!) {
             days = days + 1
         }
     }
@@ -39,9 +42,10 @@ public func dayOfYear(date: SimpleDate) -> Int {
  * The number of days elapsed between the Gregorian date 12/31/1 BC and DATE.
  * The Gregorian date Sunday, December 31, 1 BC is imaginary.
  */
-public func greg2abs(date: SimpleDate) -> Int64 {
-    let year = Int64(date.yy - 1)
-    let days = Int64(dayOfYear(date: date))
+public func greg2abs(date: Date) -> Int64 {
+    let ymd = gregCalendar.dateComponents([.year, .month, .day], from: date)
+    let year = Int64(ymd.year! - 1)
+    let days = Int64(dayOfYear(dateComponents: ymd))
     return days +   /* days this year */
         365 * year  +  /* + days in prior years */
         ((year / 4) -    /* + Julian Leap years */
@@ -55,7 +59,7 @@ public func greg2abs(date: SimpleDate) -> Int64 {
  * Clamen, Software--Practice and Experience, Volume 23, Number 4
  * (April, 1993), pages 383-404 for an explanation.
  */
-public func abs2greg(absdate: Int64) -> SimpleDate {
+public func abs2greg(absdate: Int64) -> Date {
     let d0 = absdate - 1
     let n400 = d0 / 146097
     let d1 = d0 % 146097
@@ -69,7 +73,7 @@ public func abs2greg(absdate: Int64) -> SimpleDate {
     var year = Int(400 * n400 + 100 * n100 + 4 * n4 + n1)
     
     if 4 == n100 || 4 == n1 {
-        return SimpleDate(yy: year, mm: DEC, dd: 31)
+        return DateComponents(calendar: gregCalendar, year: year, month: DEC, day: 31).date!
     } else {
         year = year + 1
         var month = 1
@@ -77,6 +81,6 @@ public func abs2greg(absdate: Int64) -> SimpleDate {
             day -= mlen
             month = month + 1
         }
-        return SimpleDate(yy: year, mm: month, dd: day)
+        return DateComponents(calendar: gregCalendar, year: year, month: month, day: day).date!
     }
 }
