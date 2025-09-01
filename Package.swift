063de02bd48c93388ed0add2c0d6b7da8bd70b00
Michael J. Radwin <michael@radwin.org>
2025-09-01 15:17:52 -0700

    Drop list of platforms in attempt to fix package resolution (#4)

--- Package.swift
+++ Package.swift
@@ -6,9 +6,6 @@ import PackageDescription
 let package = Package(
     name: "Hebcal",
     defaultLocalization: "en",
-    platforms: [
-        .macOS(.v10_14), .iOS(.v11), .watchOS(.v5)
-    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
